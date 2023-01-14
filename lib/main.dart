cc0a287705a9c41daa8dbad915d959ed691636ef
Admin <makwanadharmishtha44@gmail.com>
2023-01-14 14:22:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OpenCloseDoors(),
+    ),
+  );
+}
+
+class OpenCloseDoors extends StatefulWidget {
+  const OpenCloseDoors({Key? key}) : super(key: key);
+
+  @override
+  State<OpenCloseDoors> createState() => _OpenCloseDoorsState();
+}
+
+class _OpenCloseDoorsState extends State<OpenCloseDoors> {
+  double x = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Open-Close Doors"),
+        centerTitle: true,
+        backgroundColor: Colors.grey.shade900,
+      ),
+      body: Center(
+        child: Ink(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                if (x > 60) {
+                  x -= 20;
+                }
+              });
+            },
+            onDoubleTap: () {
+              setState(() {
+                if (x < 80) {
+                  x += 20;
+                }
+              });
+            },
+            child: Container(
+              height: 360,
+              width: 230,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.symmetric(
+                  vertical: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: x,
+                  ),
+                  horizontal: const BorderSide(
+                    color: Colors.black87,
+                    width: 60,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
